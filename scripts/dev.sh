22156e5e7bf9421cc22c0699d3af19d8765cebec
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-08-01 16:52:56 +0100

    Add mysql to dev setup

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+cleanup() {
+  docker-compose down
+  trap '' EXIT INT TERM
+  exit $err
+}
+
+trap cleanup SIGINT EXIT
+
+docker-compose up -d --force-recreate
+
 NODE_ENV=development nodemon
