68dc8e9a912a81854a2c45f96adc117899832fe7
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-08-14 10:58:56 +0100

    Add npm script for migrating prisma

---
+++ scripts/helpers.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+wait_for_server() {
+  local uri=$1
+  local delay=${2:-20}
+  local name=${3:-$1}
+
+  local i=0
+  until $(curl --max-time 0.5 --silent --fail --output /dev/null $uri); do
+    i=$(($i+1))
+    if [[ $i -gt $delay ]]; then
+      printf "${RED}${name} did not start.${NC}\n"
+      return 1
+    fi
+    printf "${ORANGE}Waiting for $name to start...${NC}\n"
+    sleep 1
+  done
+  printf "${GREEN}${name} has started.${NC}\n"
+}
+
+wait_for_mysql() {
+  wait_for_server 'localhost:3306' 10 'MYSQL'
+}
+
+start_services() {
+  docker-compose up -d --force-recreate
+  wait_for_mysql
+}
+
+stop_services() {
+  docker-compose down
+  trap '' EXIT INT TERM
+  exit $err
+}
