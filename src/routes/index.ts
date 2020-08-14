697be232a9d26be81bf5368c0975c4dd20c0b84e
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-08-14 13:46:14 +0100

    Extract status routes to own router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,53 +1,12 @@
 import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 
-import { version } from '../../package.json';
-import ApplicationError from '../errors/ApplicationError';
+import statusRouter from './status';
 
 export default (client: PrismaClient) => {
   const router = Router();
 
-  router.get('/', (_, res) => res.json({ version }));
-  router.get('/status', (_, res) => res.sendStatus(200));
-  router.get('/ready', async (_, res, next) => {
-    try {
-      await client.user.count();
-      res.sendStatus(200);
-    } catch (e) {
-      next(new ApplicationError('Unable to access db'));
-    }
-  });
-  router.get('/live', (_, res, next) => {
-    if (process.env.NODE_ENV !== 'production') {
-      return next(
-        new ApplicationError(
-          'NODE_ENV is not set to production.',
-          400,
-          'NODE_ENV_INCORRECT'
-        )
-      );
-    }
-
-    const vars = Object.getOwnPropertyNames(process.env);
-    const expected = ['SENTRY_DSN'];
-
-    const missing = expected.filter((envVar) => !vars.includes(envVar));
-    if (missing.length !== 0) {
-      return next(
-        new ApplicationError(
-          'Environment variables are not set',
-          500,
-          'UNSET_ENV_VARS',
-          {
-            missing,
-          }
-        )
-      );
-    }
-
-    return res.sendStatus(200);
-  });
-  router.use((_, res) => res.sendStatus(404));
+  router.use('/', statusRouter(client));
 
   return router;
 };
---
+++ src/routes/status.ts
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import ApplicationError from '../errors/ApplicationError';
+import { version } from '../../package.json';
+
+export default (client: PrismaClient) => {
+  const router = Router();
+
+  router.get('/', (_, res) => res.json({ version }));
+
+  router.get('/status', (_, res) => res.sendStatus(200));
+
+  router.get('/ready', async (_, res, next) => {
+    try {
+      await client.user.count();
+      res.sendStatus(200);
+    } catch (e) {
+      next(new ApplicationError('Unable to access db'));
+    }
+  });
+
+  router.get('/live', (_, res, next) => {
+    if (process.env.NODE_ENV !== 'production') {
+      return next(
+        new ApplicationError(
+          'NODE_ENV is not set to production.',
+          400,
+          'NODE_ENV_INCORRECT'
+        )
+      );
+    }
+
+    const vars = Object.getOwnPropertyNames(process.env);
+    const expected = ['SENTRY_DSN'];
+
+    const missing = expected.filter((envVar) => !vars.includes(envVar));
+    if (missing.length !== 0) {
+      return next(
+        new ApplicationError(
+          'Environment variables are not set',
+          500,
+          'UNSET_ENV_VARS',
+          {
+            missing,
+          }
+        )
+      );
+    }
+    return res.sendStatus(200);
+  });
+
+  return router;
+};
