d64ef0bdb0cd46ca18f9208a785565e45a829a10
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-08-26 12:18:12 +0100

    Update migrate scripts to work with production

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-source 'scripts/helpers.sh'
-trap stop_services SIGINT EXIT
-
-start_services
-prisma migrate save --experimental
 prisma migrate up --experimental
-stop_services
